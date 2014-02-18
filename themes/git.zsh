9ee305244ad7b569e2b0bf78730fc8feed37f941
Christian Gram Kalhauge <christian@kalhauge.dk>
2014-02-17 17:37:17 -0800

    Trimed down on the systemcalls

--- themes/git.zsh
+++ themes/git.zsh
@@ -5,53 +5,42 @@ ZSH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM=$2
 ZSH_THEME_GIT_TIME_SINCE_COMMIT_LONG=$3
 ZSH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL=$4
 
-#Customized git status, oh-my-zsh currently does not allow render dirty status before branch
-git_custom_status() {
-    local cb=$(current_branch)
-    if [ -n "$cb" ]; then
-        echo "$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_PREFIX$(current_branch)$ZSH_THEME_GIT_PROMPT_SUFFIX"
-    fi
-}
-
 # Determine the time since last commit. If branch is clean,
 # use a neutral color, otherwise colors will vary according to time.
 function git_time_since_commit() {
-    if git rev-parse --git-dir > /dev/null 2>&1; then
-    # Only proceed if there is actually a commit.
-        if [[ $(git log 2>&1 > /dev/null | grep -c "^fatal: bad default revision") == 0 ]]; then
-        # Get the last commit.
-            last_commit=`git log --pretty=format:'%at' -1 2> /dev/null`
-            now=`date +%s`
-            seconds_since_last_commit=$((now-last_commit))
+    # Get the last commit.
+    last_commit=`git log --pretty=format:'%at' -1 2> /dev/null`
+    if [[ -n "$last_commit" ]]; then
+        now=`date +%s`
+        seconds_since_last_commit=$((now-last_commit))
 
-            # Totals
-            MINUTES=$((seconds_since_last_commit / 60))
-            HOURS=$((seconds_since_last_commit/3600))
+        # Totals
+        MINUTES=$((seconds_since_last_commit / 60))
+        HOURS=$((seconds_since_last_commit/3600))
 
-            # Sub-hours and sub-minutes
-            DAYS=$((seconds_since_last_commit / 86400))
-            SUB_HOURS=$((HOURS % 24))
-            SUB_MINUTES=$((MINUTES % 60))
+        # Sub-hours and sub-minutes
+        DAYS=$((seconds_since_last_commit / 86400))
+        SUB_HOURS=$((HOURS % 24))
+        SUB_MINUTES=$((MINUTES % 60))
 
-            if [[ -n $(git status -s 2> /dev/null) ]]; then
-                if [ "$MINUTES" -gt 30 ]; then
-                    COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_LONG"
-                elif [ "$MINUTES" -gt 10 ]; then
-                    COLOR="$ZSH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM"
-                else
-                    COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_SHORT"
-                fi
+        if [[ -n $(parse_git_dirty) ]]; then
+            if [ "$MINUTES" -gt 30 ]; then
+                COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_LONG"
+            elif [ "$MINUTES" -gt 10 ]; then
+                COLOR="$ZSH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM"
             else
-                COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL"
+                COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_SHORT"
             fi
-            
-            if [ "$HOURS" -gt 24 ]; then
-                echo "$COLOR${DAYS}d${SUB_HOURS}h${SUB_MINUTES}m"
-            elif [ "$MINUTES" -gt 60 ]; then
-                echo "$COLOR${HOURS}h${SUB_MINUTES}m"
-            else
-                echo "$COLOR${MINUTES}m"
-            fi
+        else
+            COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL"
+        fi
+        
+        if [ "$HOURS" -gt 24 ]; then
+            echo "$COLOR${DAYS}d${SUB_HOURS}h${SUB_MINUTES}m"
+        elif [ "$MINUTES" -gt 60 ]; then
+            echo "$COLOR${HOURS}h${SUB_MINUTES}m"
+        else
+            echo "$COLOR${MINUTES}m"
         fi
     fi
 }
